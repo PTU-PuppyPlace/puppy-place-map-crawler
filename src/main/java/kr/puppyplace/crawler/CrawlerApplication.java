1b854dab0934fd7c8d7d149884f51702610eb5b4
RokHyung Son <fhrgud1264@gmail.com>
2024-06-06 18:38:24 +0900

    feat: 모든 Entity의 기반이 되는 BaseEntity 생성

--- src/main/java/kr/puppyplace/crawler/CrawlerApplication.java
+++ src/main/java/kr/puppyplace/crawler/CrawlerApplication.java
@@ -2,8 +2,10 @@ package kr.puppyplace.crawler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CrawlerApplication {
 
     public static void main(String[] args) {
